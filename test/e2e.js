604b7839df01d9ddf63c77f40e9211657a388072
Naoyuki Sogo <sogo@longtail-software.co.jp>
2019-04-24 21:20:48 +0000

    PBSPro will be installed by ansible on  each node

--- test/e2e.js
+++ test/e2e.js
@@ -77,7 +77,7 @@ describe("create and destroy cluster", async function() {
   const order = {
     provider: "aws",
     numNodes: 3,
-    InstanceType: "t2.nano",
+    InstanceType: "t2.micro",
     os: "ubuntu16",
     batch: "PBSpro",
     region: "ap-northeast-1",
@@ -88,7 +88,7 @@ describe("create and destroy cluster", async function() {
     clusterSchema.required.push("privateKey");
   }
 
-  it.only("should create cluster", async()=>{
+  it("should create cluster", async()=>{
     cluster = await create(order);
     expect(cluster).to.be.jsonSchema(clusterSchema);
     expect(cluster.childNodes).to.have.lengthOf(order.numNodes - 1);
@@ -109,13 +109,15 @@ describe("create and destroy cluster", async function() {
     expect(output).to.be.always.calledWithMatch(headnode);
 
     //wait for finish cloud-init
-    await arssh.watch("(tail /var/log/cloud-init-output.log && date) >&2 && cloud-init status", { out: /done|error|disabled/ }, 60000, 60, {}, console.log, console.log);
-    
-    //await arssh.exec("sudo cat /var/log/cloud-init-output.log", {}, console.log, console.log); //for debug
+    try {
+      await arssh.watch("(sudo tail -n5 /var/log/cloud-init-output.log 1>&2) && date && cloud-init status", { out: /done|error|disabled/ }, 60000, 30, {}, console.log, console.log);
+    } finally {
+      await arssh.exec("sudo cat /var/log/cloud-init-output.log", {}, console.log, console.log); //for debug
     //await arssh.exec("sudo head /etc/ssh/sshd_config", {}, console.log, console.log); //for debug
     //await arssh.exec("sudo cat /etc/ssh/shosts.equiv", {}, console.log, console.log); //for debug
     //await arssh.exec("pbsnodes -a", {}, console.log, console.log); //for debug
     //await arssh.exec("sudo cat /var/lib/cloud/instance/scripts/runcmd", {}, console.log, console.log); //for debug
+    }
 
     //check ssh login from head node to child nodes
     for (const child of cluster.childNodes) {
@@ -139,7 +141,7 @@ describe("create and destroy cluster", async function() {
 
     //check batch server
     output.reset();
-    const numJob = cluster.childNodes.length+5;
+    const numJob = cluster.childNodes.length + 5;
     await arssh.exec(`for i in \`seq ${numJob}\`; do qsub run.sh; done`, {}, output, output);
     expect(output).to.be.callCount(numJob);
     expect(output).to.be.always.calledWithMatch(headnode);
